0872345aac7869feb8ee73fc3cbb34e980e9dd0c
Building Plan EC&D <building.plan.ecd@gmail.com>
2024-10-26 16:54:52 +0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const cacheName = 'app-cache-v1';
+const assetsToCache = [
+  '',
+  'index.html',
+  'index.css',
+  'index.js',
+  'logo.jpg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll(assetsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
