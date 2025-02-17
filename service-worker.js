478f5221b035c1756db35e9e2e6bc80e37cc64cf
Building Plan EC&D <building.plan.ecd@gmail.com>
2025-02-17 10:42:22 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open('app').then((cache) => {
-      return cache.addAll(['manik.jpg','vpdone.gif','screenshot2.png','lock2.gif','screenshot.png','suc3.gif','suc4.gif','logo.jpg','logo.png','lock.gif','index2.gif','fall.gif','gift.gif','suc2.gif','suc.gif','lsuc.gif','msuc.gif','edt.gif','udc.gif','done.gif','mail.gif','index.gif']);
+      return cache.addAll(['logo.gif','manik.jpg','vpdone.gif','screenshot2.png','lock2.gif','screenshot.png','suc3.gif','suc4.gif','logo.jpg','logo.png','lock.gif','index2.gif','fall.gif','gift.gif','suc2.gif','suc.gif','lsuc.gif','msuc.gif','edt.gif','udc.gif','done.gif','mail.gif','index.gif']);
     })
   );
 });
